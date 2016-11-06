11530136ab1683cfe9f5580d10bd3f9611bacc9e
phpprogram <msummers@live.esu.edu>
2016-11-06 17:45:08 -0500

    Add files via upload

--- FormHandlingV2.php
+++ FormHandlingV2.php
@@ -2,8 +2,9 @@
 <html>  
 <body>
 
-<form action="welcome.php" method="post">
+<form action="welcomeV2.php" method="post">
 Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
 <input type="submit">
 </form>
 
